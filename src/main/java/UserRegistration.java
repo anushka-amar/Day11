64a6eb0ba4b263ab048f300340f10a2023dfcefc
Anushka <anushkamar1102@gmail.com>
2023-12-05 22:47:19 +0530

    Refactored: email validator using regex

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -2,6 +2,7 @@ public class UserRegistration {
 
     private String firstName;
     private String lastname;
+    private String email;
 
     public void setFirstName(String firstName){
         if(isValidFirstName(firstName)){
@@ -17,18 +18,33 @@ public class UserRegistration {
         }
         System.out.println("Enter valid last name");
     }
+
+    public void setEmail(String email){
+        if(isValidEmail(email)){
+            this.email = email;
+            return;
+        }
+        System.out.println("Email address invalid");
+    }
     public boolean isValidFirstName(String firstName){
         String test = "^[A-Z][a-zA-z]{2,}$";
         return firstName.matches(test);
     }
+
+    public boolean isValidEmail(String email){
+        String test = "^[a-z]+(\\.[a-z]+)*@([a-z]+\\.)+[a-z]{2,7}";
+        return email.matches(test);
+    }
     public void displayUser(){
         System.out.println("Name: "+firstName+" "+lastname);
+        System.out.println("Email: "+email);
     }
 
     public static void main(String[] args) {
         UserRegistration user = new UserRegistration();
         user.setFirstName("Anushka");
         user.setLastName("Amar");
+        user.setEmail("anushka.amar@java.gmail.com");
         user.displayUser();
     }
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -5,11 +5,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserRegistrationTest {
 
     @Test
+    public void emailValidator(){
+        assertTrue(isValidEmail("abc.xyz@bl.co.in"));
+        assertFalse(isValidEmail("abc@.in")); //as subdomain is mandatory
+        assertFalse(isValidEmail("abc.@.in"));
+    }
+    @Test
     public void firstNameValidator(){
         assertFalse(isValidFirstName("an"));
         assertFalse(isValidFirstName("anushka"));
         assertFalse(isValidFirstName("Anu@sh"));
         assertTrue(isValidFirstName("John"));
+    }
+
+
+    public boolean isValidEmail(String email){
+        String test = "^[a-z]+(\\.[a-z]+)*@([a-z]+\\.)+[a-z]{2,7}";
+        return email.matches(test);
     }
     public boolean isValidFirstName(String firstName){
         String test = "^[A-Z][a-zA-z]{2,}$";
